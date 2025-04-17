312078f6aa4069fd3cf8eaf1b0012b5c3cf99c9a
allnil <nil.medvedev17@gmail.com>
2025-04-17 15:58:43 +0100

    remove exex-wvm-da

--- templates/exex-wvm-da/src/utils.rs
+++ templates/exex-wvm-da/src/utils.rs
@@ -1,9 +1,9 @@
-use brotlic::CompressorWriter;
-use std::io::Write;
+// use brotlic::CompressorWriter;
+// use std::io::Write;
 
-pub fn to_brotli(data: Vec<u8>) -> Vec<u8> {
-    let buff: Vec<u8> = vec![];
-    let mut compressor = CompressorWriter::new(buff);
-    compressor.write_all(data.as_slice()).unwrap();
-    compressor.into_inner().unwrap()
-}
+// pub fn to_brotli(data: Vec<u8>) -> Vec<u8> {
+//     let buff: Vec<u8> = vec![];
+//     let mut compressor = CompressorWriter::new(buff);
+//     compressor.write_all(data.as_slice()).unwrap();
+//     compressor.into_inner().unwrap()
+// }
